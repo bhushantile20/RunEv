b871e959911ff3edbcd4ded3ddf3d13536869cb5
Bhushan TIle <113130816+bhushantile20@users.noreply.github.com>
2025-07-19 23:20:21 +0530

    Update user.service.js

--- Frontend/src/services/user.service.js
+++ Frontend/src/services/user.service.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import authHeader from './auth-header';
 
-const API_URL = 'localhost:5001/user/login';
+const API_URL = 'https://runev-4.onrender.com/user/login';
 
 class UserService {
   getPublicContent() {
